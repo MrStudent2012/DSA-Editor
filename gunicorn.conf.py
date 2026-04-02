d2b65f0f032b1b6548f45b16c1ed5836e3429a85
Deepak-Menan-R <deepakmenan2012@gmail.com>
2026-04-02 16:31:35 +0530

    Config file for render and socket

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,7 @@
 import os
 
 bind = f"0.0.0.0:{os.environ.get('PORT', '10000')}"
-worker_class = "eventlet"
+worker_class = "gthread"
 workers = int(os.environ.get("WEB_CONCURRENCY", "1"))
+threads = 2
 timeout = int(os.environ.get("GUNICORN_TIMEOUT", "120"))
